49f199292307781f71a42e00f7e2c1eae30786ad
Odunayo <okebunmiodunayo@gmail.com>
2021-04-16 14:48:13 +0200

    setup mongodb connection

--- src/server/server.js
+++ src/server/server.js
@@ -3,35 +3,42 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import Debug from 'debug';
-import logger from 'morgan';
+import logger from '../common/services/logger';
 import router from './routes';
+import { connectToDB, disconnectDB } from './db';
 
 if (process.env.NODE_ENV !== 'production') dotenv.config();
 
 const app = express();
-const debug = Debug(process.env.DEBUG);
 
 app.use(cors());
-app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.use(express.json());
 
 process.on('uncaughtException', (err) => {
-  debug(err.stack);
+  logger.warn(err.stack);
   process.exit(1);
 });
-
+(async () => {
+  await disconnectDB();
+})();
 // Handle non existing routes
 app.all('*', (req, res) => res.status(404).json({
   status: 404,
   error: 'Page not found',
 }));
+(async () => {
+  try {
+    await connectToDB();
+  } catch (err) {
+    logger.warn(err);
+  }
+})();
 
 const server = app.listen(process.env.PORT || 5000, () => {
-  debug(`Listening on port ${server.address().port}`);
+  logger.info(`Listening on port ${server.address().port}`);
 });
 
 export default app;
