a813118d688ed0438de974986d87bf77c34454d2
kengneruphine <ruphinekengne@gmail.com>
2021-08-27 11:44:13 +0100

    feat:adding slug attribute to the event schema

--- src/data/event/event.schema.js
+++ src/data/event/event.schema.js
@@ -1,17 +1,23 @@
 import mongoose, { Schema } from 'mongoose';
-import { uuidv4, trimmedString } from '../utils/schema.utils';
+import Slug from 'mongoose-slug-updater';
+import { uuidv4, trimmedString, timestamps } from '../utils/schema.utils';
 
 const EventSchema = new Schema({
   _id: { ...uuidv4 },
   name: {
     type: String,
     required: true,
-    unique: true,
-    ...trimmedString,
   },
   location: {
     type: String,
     required: true,
+  },
+  slug: {
+    ...trimmedString,
+    slug: 'name',
+    index: true,
+    unique: true,
+    slugPaddingSize: 4,
   },
   description: {
     type: String,
@@ -20,12 +26,16 @@ const EventSchema = new Schema({
     type: Date,
     required: true,
   },
-  endtime: {
+  endTime: {
     type: Date,
     required: true,
   },
 
+},
+{
+  ...timestamps,
 });
 
+mongoose.plugin(Slug);
 const Event = mongoose.model('Event', EventSchema);
 export default Event;
