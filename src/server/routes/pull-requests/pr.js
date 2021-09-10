6f95eada6c338a340d132822f2059f5a2acb4f55
kengneruphine <ruphinekengne@gmail.com>
2021-09-10 10:49:15 +0100

    fix routes

--- src/server/routes/pull-requests/pr.js
+++ src/server/routes/pull-requests/pr.js
@@ -13,7 +13,7 @@ PullRequestsRouter.post('/', AuthMiddleware, rbac('user'), validator(submitPRVal
 
 PullRequestsRouter.get('/', AuthMiddleware, getPullRequests);
 
-PullRequestsRouter.get('/:prID', AuthMiddleware, getOnePullRequest);
-PullRequestsRouter.put('/prID', AuthMiddleware, rbac('admin'), updatePullRequests);
+PullRequestsRouter.get('/:prId', AuthMiddleware, getOnePullRequest);
+PullRequestsRouter.put('/prId', AuthMiddleware, rbac('admin'), updatePullRequests);
 
 export default PullRequestsRouter;
