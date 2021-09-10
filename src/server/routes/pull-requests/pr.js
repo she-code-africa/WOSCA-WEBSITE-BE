309ba07b7add71f12c3d23dcaced708c3e1dc469
Odunayo <okebunmiodunayo@gmail.com>
2021-09-10 10:30:32 +0200

    added routes for PR

--- src/server/routes/pull-requests/pr.js
+++ src/server/routes/pull-requests/pr.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { submitPullRequest } from '../../controllers/pull-requests/pr.controller';
+import {
+  submitPullRequest, getOnePullRequest, getPullRequests, updatePullRequests,
+} from '../../controllers/pull-requests/pr.controller';
 import validator from '../../middlewares/validator';
 import { submitPRValidation } from '../../controllers/pull-requests/pr.validation';
 import AuthMiddleware from '../../middlewares/authorization';
@@ -8,5 +10,10 @@ import rbac from '../../middlewares/rbac';
 const PullRequestsRouter = Router();
 
 PullRequestsRouter.post('/', AuthMiddleware, rbac('user'), validator(submitPRValidation), submitPullRequest);
+
+PullRequestsRouter.post('/', AuthMiddleware, getPullRequests);
+
+PullRequestsRouter.post('/', AuthMiddleware, getOnePullRequest);
+PullRequestsRouter.post('/', AuthMiddleware, rbac('admin'), updatePullRequests);
 
 export default PullRequestsRouter;
